13b3306341e35f96942dba6277675085f2019666
Akash007AD <debnath2004akash@gmail.com>
2026-02-01 12:55:33 +0530

    Deployement issue fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { Layout } from './components/Layout';
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { Skills } from './components/Skills';
-import { Experience } from './components/Experience';
-import { Projects } from './components/Projects';
-import { Achievements } from './components/Achievements';
-import { CodingProfile } from './components/CodingProfile';
-import { Contact } from './components/Contact';
+import { Layout } from "./components/Layout";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Experience } from "./components/Experience";
+import { Projects } from "./components/Projects";
+import { Achievements } from "./components/Achievements";
+import { CodingProfile } from "./components/CodingProfile";
+import { Contact } from "./components/Contact";
 
 function App() {
-    return (
-        <Layout>
-            <div className="space-y-20 pb-20">
-                <Hero />
-                <About />
-                <Skills />
-                <Experience />
-                <Projects />
-                <Achievements />
-                <CodingProfile />
-                <Contact />
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className="space-y-20 pb-20">
+        <Hero />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Achievements />
+        <CodingProfile />
+        <Contact />
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
